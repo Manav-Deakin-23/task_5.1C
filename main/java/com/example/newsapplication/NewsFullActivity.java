60a246963d9c321e4a5ea9d6d71dc873f380f417
Manav Mehta <139406828+Manav-Deakin-23@users.noreply.github.com>
2024-04-22 14:50:18 +1000

    Add files via upload

---
+++ main/java/com/example/newsapplication/NewsFullActivity.java
@@ -0,0 +1,38 @@
+package com.example.newsapplication;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NewsFullActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_news_full);
+
+        String url = getIntent().getStringExtra("url");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
